dab2228a52d43dcba1cc967c0b8d478143bd03f8
Hafizh Fauzan <hafizhfauzan3030@gmail.com>
2024-05-14 21:13:34 +0700

    TugasPertemuan11

--- src/App.vue
+++ src/App.vue
@@ -10,41 +10,16 @@
     </header>
 
     <div class="container">
-    <div v-if="currentView === 'todos'">
-      <h2>Todos</h2>
-      <form @submit.prevent="addTodo">
-        <input type="text" v-model="newTodo" placeholder="Tambah kegiatan baru" required>
-        <button @click="confirmAction('Add', index)" type="submit">Tambah</button>
-      </form>
-      <div v-for="(todo, index) in filteredTodos" :key="index">
-        <!-- Menggunakan @click untuk updateTodo -->
-        <input type="checkbox" v-model="todo.completed" @click="updateTodo(index)">
-        <span :class="{ 'completed': todo.completed }">{{ todo.text }}</span>
-        <button @click="confirmAction('Remove', index)">Remove</button>
-      </div>
-      <!-- Tombol filter untuk menampilkan hanya kegiatan yang belum selesai -->
-      <button @click="filterTodos">{{ showCompleted ? 'Filter Belum Selesai' : 'Tampilkan Semua' }}</button>
-      </div>
-      <!-- Bagian lain dari aplikasi -->
-      <div v-else-if="currentView === 'posts'">
-        <h2>Posts</h2>
-        <select v-model="selectedUser" @change="fetchPosts">
-          <option v-for="user in users" :key="user.id" :value="user.id">{{ user.name }}</option>
-        </select>
-
-        <div v-if="loading">Loading...</div>
-        <div v-else>
-          <div v-for="post in posts" :key="post.id">
-            <h3>{{ post.title }}</h3>
-            <p class="center-text">{{ post.body }}</p>
-          </div>
-        </div>
-      </div>
+      <Todos v-if="currentView === 'todos'" :todos="todos" :newTodo="newTodo" :showCompleted="showCompleted" @addTodo="addTodo" @updateTodo="updateTodo" @removeTodo="removeTodo" @filterTodos="filterTodos" @confirmAction="confirmAction" />
+      <Post v-else-if="currentView === 'posts'" :users="users" :selectedUser="selectedUser" :posts="posts" :loading="loading" @fetchPosts="fetchPosts" />
     </div>
   </div>
 </template>
 
 <script>
+import Todos from './Todos.vue';
+import Post from './Post.vue';
+
 export default {
   data() {
     return {
@@ -61,19 +36,16 @@ export default {
       loading: false
     };
   },
-  computed: {
-    // Menggunakan computed property untuk memfilter kegiatan yang belum selesai
-    filteredTodos() {
-      if (this.showCompleted) {
-        return this.todos;
-      } else {
-        return this.todos.filter(todo => !todo.completed);
-      }
-    }
+
+  components: {
+    Todos,
+    Post
   },
+
   mounted() {
     this.fetchUsers();
   },
+
   methods: {
     switchView(view) {
       this.currentView = view;
@@ -85,17 +57,11 @@ export default {
       }
     },
     updateTodo(index) {
-      // Memeriksa apakah sudah selesai, jika ya, ubah menjadi tidak selesai
-      if (this.todos[index].completed) {
-        this.todos[index].completed = false;
-      } else {
-        this.todos[index].completed = true;
-      }
+      this.todos[index].completed = !this.todos[index].completed;
     },
     removeTodo(index) {
       this.todos.splice(index, 1);
     },
-    // Mengubah status tombol filter dan menampilkan semua kegiatan
     filterTodos() {
       this.showCompleted = !this.showCompleted;
     },
@@ -110,7 +76,6 @@ export default {
         this.loading = false;
       }
     },
-
     confirmAction(action, index = null) {
       let message = '';
       switch (action) {
@@ -140,7 +105,6 @@ export default {
         }
       }
     },
-
     async fetchPosts() {
       this.loading = true;
       try {
@@ -156,39 +120,4 @@ export default {
 };
 </script>
 
-<style>
-header {
-  background-color: #333;
-  color: white;
-}
-
-nav ul {
-  list-style-type: none;
-  display: flex;
-}
-
-nav ul li {
-  padding: 10px;
-  cursor: pointer;
-}
-
-nav ul li:hover {
-  background-color: #555;
-}
-
-.container {
-  margin-top: 20px;
-}
-
-.completed {
-  text-decoration: line-through;
-}
-
-.active-todos {
-  background-color: rgb(95, 95, 95); /* Warna latar belakang untuk Todos saat aktif */
-}
-.active-posts {
-  background-color: rgb(95, 95, 95); /* Warna latar belakang untuk Post saat aktif */
-}
-
-</style>
+<style src="./Style.css"></style>
