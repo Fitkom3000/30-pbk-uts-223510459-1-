2d04b84a91a2616fbb84039571cee4d2e8972e96
Hafizh Fauzan <hafizhfauzan3030@gmail.com>
2024-06-25 09:31:58 +0700

    Perbaikan Logika

--- src/App.vue
+++ src/App.vue
@@ -2,30 +2,51 @@
   <div id="app">
     <header>
       <nav>
-        <ul style="display: flex;">
-          <li :class="{ 'active-todos': currentView === 'todos' }" @click="switchView('todos')">Todos</li>
-          <li :class="{ 'active-posts': currentView === 'posts' }" @click="switchView('posts')">Post</li>
+        <ul style="display: flex">
+          <li :class="{ 'active-todos': currentView === 'todos' }" @click="switchView('todos')">
+            Todos
+          </li>
+          <li :class="{ 'active-posts': currentView === 'posts' }" @click="switchView('posts')">
+            Post
+          </li>
         </ul>
       </nav>
     </header>
 
     <div class="container">
-      <Todos v-if="currentView === 'todos'" :todos="todos" :newTodo="newTodo" :showCompleted="showCompleted" @addTodo="addTodo" @updateTodo="updateTodo" @removeTodo="removeTodo" @filterTodos="filterTodos" @confirmAction="confirmAction" />
-      <Post v-else-if="currentView === 'posts'" :users="users" :selectedUser="selectedUser" :posts="posts" :loading="loading" @fetchPosts="fetchPosts" />
+      <Todos
+        v-if="currentView === 'todos'"
+        :todos="todos"
+        :newTodo="newTodo"
+        :showCompleted="showCompleted"
+        @addTodo="addTodo"
+        @updateTodo="updateTodo"
+        @removeTodo="removeTodo"
+        @filterTodos="filterTodos"
+        @confirmAction="confirmAction"
+      />
+      <Post
+        v-else-if="currentView === 'posts'"
+        :users="users"
+        :selectedUser="selectedUser"
+        :posts="posts"
+        :loading="loading"
+        @fetchPosts="fetchPosts"
+      />
     </div>
   </div>
 </template>
 
 <script>
-import Todos from './Todos.vue';
-import Post from './Post.vue';
+import Todos from './components/Todos.vue'
+import Post from './components/Post.vue'
 
 export default {
   data() {
     return {
       todos: [
-        { text: 'Susah banget', completed: false },
-        { text: 'Woila', completed: false }
+        { text: 'Bismillah', completed: false },
+        { text: 'UAS PBK', completed: false }
       ],
       newTodo: '',
       currentView: 'todos',
@@ -34,7 +55,7 @@ export default {
       selectedUser: null,
       posts: [],
       loading: false
-    };
+    }
   },
 
   components: {
@@ -43,81 +64,83 @@ export default {
   },
 
   mounted() {
-    this.fetchUsers();
+    this.fetchUsers()
   },
 
   methods: {
     switchView(view) {
-      this.currentView = view;
+      this.currentView = view
     },
     addTodo() {
       if (this.newTodo.trim() !== '') {
-        this.todos.push({ text: this.newTodo, completed: false });
-        this.newTodo = '';
+        this.todos.push({ text: this.newTodo, completed: false })
+        this.newTodo = ''
       }
     },
     updateTodo(index) {
-      this.todos[index].completed = !this.todos[index].completed;
+      this.todos[index].completed = !this.todos[index].completed
     },
     removeTodo(index) {
-      this.todos.splice(index, 1);
+      this.todos.splice(index, 1)
     },
     filterTodos() {
-      this.showCompleted = !this.showCompleted;
+      this.showCompleted = !this.showCompleted
     },
     async fetchUsers() {
-      this.loading = true;
+      this.loading = true
       try {
-        const response = await fetch('https://jsonplaceholder.typicode.com/users');
-        this.users = await response.json();
+        const response = await fetch('https://jsonplaceholder.typicode.com/users')
+        this.users = await response.json()
       } catch (error) {
-        console.error('Error fetching users:', error);
+        console.error('Error fetching users:', error)
       } finally {
-        this.loading = false;
+        this.loading = false
       }
     },
     confirmAction(action, index = null) {
-      let message = '';
+      let message = ''
       switch (action) {
         case 'Remove':
-          message = `Apakah Anda yakin ingin menghapus kegiatan ini?`;
-          break;
+          message = 'Apakah Anda yakin ingin menghapus kegiatan ini?'
+          break
         case 'Add':
-          message = `Apakah Anda yakin ingin menambah kegiatan ini?`;
-          break;
+          message = 'Apakah Anda yakin ingin menambah kegiatan ini?'
+          break
         default:
-          break;
+          break
       }
 
       if (window.confirm(message)) {
         switch (action) {
           case 'Update':
-            this.updateTodo(index);
-            break;
+            this.updateTodo(index)
+            break
           case 'Remove':
-            this.removeTodo(index);
-            break;
+            this.removeTodo(index)
+            break
           case 'Filter':
-            this.filterTodos();
-            break;
+            this.filterTodos()
+            break
           default:
-            break;
+            break
         }
       }
     },
     async fetchPosts() {
-      this.loading = true;
+      this.loading = true
       try {
-        const response = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${this.selectedUser}`);
-        this.posts = await response.json();
+        const response = await fetch(
+          `https://jsonplaceholder.typicode.com/posts?userId=${this.selectedUser}`
+        )
+        this.posts = await response.json()
       } catch (error) {
-        console.error('Error fetching posts:', error);
+        console.error('Error fetching posts:', error)
       } finally {
-        this.loading = false;
+        this.loading = false
       }
     }
   }
-};
+}
 </script>
 
 <style src="./Style.css"></style>
---
+++ src/components/Post.vue
@@ -0,0 +1,28 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+  <div>
+    <h2>Posts</h2>
+    <select v-model="$parent.selectedUser" @change="$emit('fetchPosts')">
+      <option value="null" disabled selected>Silahkan pilih user</option>
+      <option v-for="user in users" :key="user.id" :value="user.id">{{ user.name }}</option>
+    </select>
+    <div v-if="loading">Loading....</div>
+    <div v-else>
+      <div v-for="post in posts" :key="post.id">
+        <h3>{{ post.title }}</h3>
+        <p class="center-text">{{ post.body }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    users: Array,
+    selectedUser: Number,
+    posts: Array,
+    loading: Boolean
+  }
+}
+</script>
